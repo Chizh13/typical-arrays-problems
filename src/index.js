183598858dff56b131e886e3eadd7535643ed666
Chizh13 <antonchizhevich48@fmail.com>
2020-02-23 11:44:54 +0300

    fix: task performance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+	var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+    console.log(min);
 }
 
 exports.max = function max (array) {
-  return 0;
+	 var max = array[0]; // берем первый элемент массива
+    for (var i = 0; i < array.length; i++) { // переберем весь массив
+        // если элемент больше, чем в переменной, то присваиваем его значение переменной
+        if (max < array[i]) max = array[i]; 
+    }
+    // возвращаем максимальное значение
+    return max;
+    console.log(max);
+}
+exports.avg = function avg (array) {
+	
+	var i,
+           sum = 0, 
+           len = array.length; 
+       for (i = 0; i < len; i++) { 
+           sum += array[i]; 
+       }
+       return sum / len;
+   
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
